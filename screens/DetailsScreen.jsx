cc5ef24c99e4d547c38c931233a750274abaad4c
“MonkeYor13” <monkeyor13@gmail.com>
2024-08-26 13:44:53 -0300

    continuamos ocn las apis

--- screens/DetailsScreen.jsx
+++ screens/DetailsScreen.jsx
@@ -1,64 +1,58 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Image, FlatList, StyleSheet, ActivityIndicator } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView, ActivityIndicator, ScrollView, Image, StatusBar } from 'react-native';
 import axios from 'axios';
+
+
 
 export default function DetailsScreen() {
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState('')
 
   useEffect(() => {
-    // URL de la API ficticia
-    // axios.get('https://jsonplaceholder.typicode.com/photos')
-    //   .then(response => {
-    //     // Solo tomamos las primeras 10 imágenes para simplificar
-    //     setData(response.data.slice(0, 10));
-    //     setLoading(false);
-    //   })
-    //   .catch(error => {
-    //     console.error('Error fetching data:', error);
-    //     setLoading(false);
-    //   });
-  }, []);
+    const fetchData = async () => {
+      try {
+        const res = await axios.get('ttps://jsonplaceholder.typicode.com/photos?_limit=5');
+        setData(res.data);
+        setLoading(false)
+      } catch (error) {
+        console.log('error en la api', error)
+        setError('Hay un Error!')
+      }
+    };
+    // Llamar a la función asincrónica
+    fetchData();
+  }, []); // El array vacío asegura que el efecto solo se ejecute una vez, cuando el componente se monta.
 
   if (loading) {
-    return <ActivityIndicator size="large" color="#0000ff" />;
+    return (
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#f5f5f5', paddingTop: StatusBar.currentHeight, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size={'large'} color='red' />
+        <Text>Cargando...</Text>
+        <Text>{error}</Text>
+      </SafeAreaView>
+    )
   }
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={data}
-        keyExtractor={item => item.id.toString()}
-        renderItem={({ item }) => (
-          <View style={styles.item}>
-            <Image
-              source={{ uri: item.url }}
-              style={styles.image}
-            />
-            <Text style={styles.title}>{item.title}</Text>
+    <ScrollView>
+      <Text>Datos de la API:</Text>
+      {
+        data.map((item) => (
+          <View key={item.id}>
+            <Text>Titulo es : {item.title}</Text>
+            <Image source={{ uri: item.url }} style={styles.image} />
           </View>
-        )}
-      />
-    </View>
+        ))
+      }
+
+    </ScrollView>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 50,
-    backgroundColor: '#f0f0f0',
-  },
-  item: {
-    marginBottom: 20,
-    alignItems: 'center',
-  },
   image: {
     width: 200,
     height: 200,
-    marginBottom: 10,
-  },
-  title: {
-    fontSize: 16,
-    textAlign: 'center',
+    borderRadius: 10,
   },
 });
